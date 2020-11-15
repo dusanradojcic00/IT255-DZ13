6dc4520d85415067426ca2cdfd1ed8f502ee0abd
dusanradojcic00 <dusanradojcic00@gmail.com>
2020-11-15 20:02:20 +0100

    IT255-DZ13

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,75 @@
-import { Component } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, HostBinding, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogComponent } from './dialogs/dialog/dialog.component';
+import { Room } from './models/room.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy, DoCheck, OnChanges, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked {
   title = 'IT255-DZ13';
+  rooms: Room[];
+  @HostBinding('attr.class') cssClass = "container";
+  constructor(private dialog: MatDialog) { }
+
+  ngOnInit(): void {
+    this.rooms = [
+      new Room("Soba 1", 500)
+    ]
+  }
+
+  addRoom(name: HTMLInputElement, price: HTMLInputElement) {
+    this.rooms.push(new Room(name.value, parseInt(price.value)));
+    name.value = price.value = '';
+    return false;
+  }
+
+  deleteRoom(room: Room) {
+    const index = this.rooms.indexOf(room);
+    this.rooms.splice(index, 1);
+  }
+
+  editRoom(room: Room) {
+    const dialogRef = this.dialog.open(DialogComponent, {
+      data: {
+        room: room
+      }
+    });
+    dialogRef.afterClosed().subscribe((newRoom) => {
+      room.name = newRoom.name;
+      room.price = newRoom.price;
+    })
+  }
+
+  ngDoCheck(): void {
+    console.log('DoCheck');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('AfterContentInit');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit fired');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('AfterContentChecked');
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('AfterViewChecked');
+  }
+
+  ngOnDestroy(): void {
+    console.log('OnDestroy');
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(`OnChanges: ${changes}`);
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgRoomForDirective } from './ng-room-for.directive';
+import { RoomComponent } from './components/room/room.component';
+import { DialogComponent } from './dialogs/dialog/dialog.component';
+
+import { MatDialogModule } from "@angular/material/dialog";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgRoomForDirective,
+    RoomComponent,
+    DialogComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    MatDialogModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [
+    DialogComponent
+  ],
 })
 export class AppModule { }
